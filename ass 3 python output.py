69078a218de7cf5f3bd11286161eef0cc116a5a9
Harmanpreet Singh <96690105+Harmanpreet-Singh619@users.noreply.github.com>
2022-02-05 10:45:23 +0530

    Add files via upload

---
+++ ass 3 python output.py
@@ -0,0 +1,117 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Harmanpreet Singh/AppData/Local/Programs/Python/Python310/ojawof.py
+ 			 Question 1 
+
+
+Enter string: I am Harmanpreet Singh. I am smart and I am honest,
+
+Number of each word/character in string is: {'i': 3, 'am': 3, 'harmanpreet': 1, 'singh.': 1, 'smart': 1, 'and': 1, 'honest,': 1} 
+
+			 Question 2 
+
+
+Enter the date: 31
+Enter the month: 03
+Enter the year: 2021
+
+Next day is: 1/4/2021
+
+			 Question 3 
+
+Enter the total number of terms you gonna enter: 3
+
+Enter the 1 number: 6
+Enter the 2 number: 7
+Enter the 3 number: 8
+
+[(6, 36), (7, 49), (8, 64)]
+
+			 Question 4 
+
+
+Enter the grade number: 9
+
+Your grade is 'A' and Excellent Performance
+
+			 Question 5 
+
+
+
+Q.5
+The pattern is printed below:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+			 Question 6 
+
+Enter student name:Harmanpreet Singh
+Enter student SID:21105053
+Enter Y to continue or N to end:Y
+Enter student name:Akshat Dutta
+Enter student SID:21105093
+Enter Y to continue or N to end:Y
+Enter student name:Samaira
+Enter student SID:21104035
+Enter Y to continue or N to end:N
+
+ Q6(a)
+The Dictionary containing {'SID':'Name'} is shown below
+{21105053: 'Harmanpreet Singh', 21105093: 'Akshat Dutta', 21104035: 'Samaira'}
+
+(b)
+
+Sorted dict using students names is:  {21105093: 'Akshat Dutta', 21105053: 'Harmanpreet Singh', 21104035: 'Samaira'}
+
+(c)
+
+The Dictionary after sorting according to SID:
+{21104035: 'Samaira', 21105053: 'Harmanpreet Singh', 21105093: 'Akshat Dutta'}
+
+(d)
+)
+Enter SID to get name of student:21105053
+Name of student with SID 21105053 is Harmanpreet Singh.
+
+			 Question 7 
+
+
+Enter the the max number of terms you want to print: 5
+
+Fibonacci sequence with 5 terms:
+0
+1
+1
+2
+3
+
+Average of fab series is:  1.4
+
+			 Question 8 
+
+
+	 Part a 
+
+Set with all the elements of Set 1 and Set 2 but not both is: {1, 3, 5, 6, 8}
+
+	 (b) 
+
+The only non-repeating elements are :  {3, 6, 8, 9, 13, 17}
+
+	 (c) 
+
+The set of elements that are exactly two of the sets Set1, Set2 and Set3:  {1, 2, 4, 5}
+
+	 (d) 
+
+The set having all int less than equal to 10 except that are not in set_1:  {6, 7, 8, 9, 10}
+
+	 (e) 
+
+The set having all int less than equal to 10 except that are not in all the sets:  {10, 7}
